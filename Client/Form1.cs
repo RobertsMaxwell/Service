b888281e5acf702b595240d1863169481511ce01
Sebastian Roberts <tiansebas09@gmail.com>
2020-07-28 18:04:28 -0500

    Client Simple GUI

--- Client/Form1.cs
+++ Client/Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +17,34 @@ namespace Client
         public Form1()
         {
             InitializeComponent();
+        } 
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if(targetIP.Text == "")
+            {
+                return;
+            }
+
+            try
+            {
+                Thread thread = new Thread(new ThreadStart(ConnectToServer));
+
+                thread.Start();
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Connection Failed");
+            }
+        }
+
+        private void ConnectToServer()
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            Console.WriteLine("Establishing Connection to {0}:1738", targetIP.Text);
+            s.Connect(targetIP.Text, 1738);
+            Console.WriteLine("Connection established");
         }
     }
 }
